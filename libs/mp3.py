fc717d0f112e0cd2067d348d750e92c0894341c0
Kaveh Bakhtiyari <kbakhtiyari@yahoo.com>
2024-02-14 13:48:28 -0500

    Download Audio files

--- libs/mp3.py
+++ libs/mp3.py
@@ -4,14 +4,7 @@ import os
 
 
 def get_file_name(id):
-    if id < 10:
-        str_i = "000" + str(id)
-    elif 10 <= id < 100:
-        str_i = "00" + str(id)
-    elif 100 <= id < 1000:
-        str_i = "0" + str(id)
-
-    file_name = str_i + ".mp3"
+    file_name = f"{id:04d}.mp3"
     return file_name
 
 
