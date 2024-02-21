56dd055b0e9dcc9c01aed59f786898b49b23ab59
Kaveh Bakhtiyari <kbakhtiyari@yahoo.com>
2024-02-21 17:15:01 -0500

    Updated remote url access

--- libs/mp3.py
+++ libs/mp3.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import hafez
 import os
+import requests
 
 
 def get_file_name(id):
@@ -11,11 +12,17 @@ def get_file_name(id):
 def play_audio(id):
 
     # mp3_file_path = f'mp3/{get_file_name(id)}'
-    mp3_file_path = hafez.get_audio(id)
+    mp3_file_path = hafez.get_audio(poem=id, download=False)
 
-    if not os.path.exists(mp3_file_path):
-        hafez.download_audio(id)
+    if str(mp3_file_path).startswith("http"):
+        with requests.Session() as req:
+            download = req.get(mp3_file_path)
+            if download.status_code == 200:
+                audio_bytes = download.content
+            else:
+                st.error("There was an issue to download the sound.")
+    else:
+        audio_file = open(mp3_file_path, 'rb')
+        audio_bytes = audio_file.read()
 
-    audio_file = open(mp3_file_path, 'rb')
-    audio_bytes = audio_file.read()
     st.audio(audio_bytes, format='audio / ogg')
