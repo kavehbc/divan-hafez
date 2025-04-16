ec6acec54227bd66e2425d727f46489b2e7453f0
Kaveh Bakhtiyari <kbakhtiyari@yahoo.com>
2025-04-16 06:31:42 -0400

    updated default font

--- libs/constants.py
+++ libs/constants.py
@@ -2,6 +2,6 @@ MENU_OPTIONS = {"app": "دیوان حافظ",
                 "download": "دانلود صدا",
                 "about": "درباره"}
 
-FONT_NAMES = {"yekan": "Yekan", "nastaliq": "Nastaliq", "iran_sans": "Iran Sans"}
+FONT_NAMES = {"iran_sans": "Iran Sans", "yekan": "Yekan", "nastaliq": "Nastaliq"}
 
 POEM_LAYOUT_OPTIONS = {"col1": "تک ستون", "col2": "دو ستون"}
