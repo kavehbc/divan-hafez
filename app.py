d7c160bf6d51846b66aff67747104e9d2d989b0e
Kaveh Bakhtiyari <kbakhtiyari@yahoo.com>
2025-07-13 04:48:29 -0400

    refactor main function into separate page functions for better readability and organization

--- app.py
+++ app.py
@@ -7,62 +7,71 @@ from libs.readme import show_readme
 from libs.ui import create_text, init_ui, show_poem, show_search_result
 
 
-def main():
-    st_app_menu = st.sidebar.selectbox("Main Menu", options=list(MENU_OPTIONS.keys()), index=0,
-                                       format_func=lambda x: MENU_OPTIONS[x])
-    if st_app_menu == "about":
-        show_readme()
-        st.stop()
-    elif st_app_menu == "download":
-        st.title("دانلود صدا")
-        st.markdown("___")
-        btn_download = st.button("بارگیری دکلمه تمام اشعار")
-        if btn_download:
-            st_download_progress = st.progress(0, text="Downloading...")
-            for item in hafez.download_all_audio():
-                st_download_progress.progress(item / 495, text=f"Downloading {item}...")
+def pg_about():
+    show_readme()
+    st.stop()
 
-            st_download_progress.progress(100, text=f"Download Completed.")
-    elif st_app_menu == "app":
+def pg_download():
+    st.title("دانلود صدا")
+    st.markdown("___")
+    btn_download = st.button("بارگیری دکلمه تمام اشعار")
+    if btn_download:
+        st_download_progress = st.progress(0, text="Downloading...")
+        for item in hafez.download_all_audio():
+            st_download_progress.progress(item / 495, text=f"Downloading {item}...")
 
-        st.title("دیوان حافظ")
-        st.markdown("___")
+        st_download_progress.progress(100, text=f"Download Completed.")
 
-        int_poem = st.sidebar.number_input("َPoem #", min_value=1, max_value=495, step=1)
-        btn_show_poem = st.sidebar.button("نمایش غزل")
-        str_query = st.sidebar.text_input("َSearch Query")
-        btn_search_poem = st.sidebar.button("جستجو")
+def pg_app():
+    st.title("دیوان حافظ")
+    st.markdown("___")
 
-        font_name = st.sidebar.selectbox("Font Name", options=list(FONT_NAMES.keys()), index=0,
-                                         format_func=lambda x: FONT_NAMES[x])
-        st_poem_layout = st.sidebar.selectbox("Poem Layout", options=list(POEM_LAYOUT_OPTIONS.keys()),
-                                              index=0,
-                                              format_func=lambda x: POEM_LAYOUT_OPTIONS[x])
+    int_poem = st.sidebar.number_input("َPoem #", min_value=1, max_value=495, step=1)
+    btn_show_poem = st.sidebar.button("نمایش غزل")
+    str_query = st.sidebar.text_input("َSearch Query")
+    btn_search_poem = st.sidebar.button("جستجو")
 
-        font_size = 2
-        # font_size = st.sidebar.number_input("Font Size", min_value=0.5, max_value=5.0, value=1.5, step=0.1)
+    font_name = st.sidebar.selectbox("Font Name", options=list(FONT_NAMES.keys()), index=0,
+                                        format_func=lambda x: FONT_NAMES[x])
+    st_poem_layout = st.sidebar.selectbox("Poem Layout", options=list(POEM_LAYOUT_OPTIONS.keys()),
+                                            index=0,
+                                            format_func=lambda x: POEM_LAYOUT_OPTIONS[x])
 
-        btn_fall = st.button("نمایش فال حافظ")
+    font_size = 2
+    # font_size = st.sidebar.number_input("Font Size", min_value=0.5, max_value=5.0, value=1.5, step=0.1)
 
-        if btn_fall:
-            int_poem = random.randint(1, 495)
+    btn_fall = st.button("نمایش فال حافظ")
 
-        if btn_search_poem:
-            if len(str_query) == 0:
-                st.error("جهت جستجو عبارت مورد نظر را وارد نمایید.")
-            else:
-                show_search_result(str_query, font_name=font_name, layout=st_poem_layout)
+    if btn_fall:
+        int_poem = random.randint(1, 495)
 
-        elif btn_show_poem or btn_fall:
-            show_poem(int_poem, font_name=font_name, layout=st_poem_layout)
-
+    if btn_search_poem:
+        if len(str_query) == 0:
+            st.error("جهت جستجو عبارت مورد نظر را وارد نمایید.")
         else:
-            with open('db/home.md', 'r', encoding="utf-8") as outfile:
-                md_text = outfile.read()
+            show_search_result(str_query, font_name=font_name, layout=st_poem_layout)
 
-            lst_md_text = md_text.split("\n")
-            for paragraph in lst_md_text:
-                st.markdown(create_text(paragraph, font_name="yekan"), unsafe_allow_html=True)
+    elif btn_show_poem or btn_fall:
+        show_poem(int_poem, font_name=font_name, layout=st_poem_layout)
+
+    else:
+        with open('db/home.md', 'r', encoding="utf-8") as outfile:
+            md_text = outfile.read()
+
+        lst_md_text = md_text.split("\n")
+        for paragraph in lst_md_text:
+            st.markdown(create_text(paragraph, font_name="yekan"), unsafe_allow_html=True)
+            
+def main():
+
+    pages = [
+            st.Page(pg_about, title="درباره", icon="📌"),
+            st.Page(pg_download, title="دانلود صدا", icon="📲"),
+            st.Page(pg_app, title="دیوان حافظ", icon="📖")
+        ]
+
+    pg = st.navigation(pages)
+    pg.run()
 
 
 if __name__ == '__main__':
